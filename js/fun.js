142aa4fee1dca259a91bbc66fff0fc675f1d4354
Montagab Al-Hamawy <100755840+MontagabAlHamawy@users.noreply.github.com>
2022-07-11 09:48:19 +0300

    Update fun.js

--- js/fun.js
+++ js/fun.js
@@ -12,6 +12,21 @@ function yt ( a, b , c )
     setTimeout( function () { location.reload() }, 3000 );
     f = 1;
 }
+function ytt ( a, b , c, d, e, j, g, h, i )
+{
+    title.innerHTML = `${ er[ a ] } winner`;
+        document.getElementById( 'we' + a ).style.background = '#fff';
+        document.getElementById( 'we' + b ).style.background = '#fff';
+        document.getElementById( 'we' + c ).style.background = '#fff';
+        document.getElementById( 'we' + d ).style.background = '#fff';
+        document.getElementById( 'we' + e ).style.background = '#fff';
+        document.getElementById( 'we' + j ).style.background = '#fff';
+        document.getElementById( 'we' + g ).style.background = '#fff';
+        document.getElementById( 'we' + h ).style.background = '#fff';
+        document.getElementById( 'we' + i ).style.background = '#fff';
+        setInterval( function () { title.innerHTML += '.' }, 1000 );
+    setTimeout( function () { location.reload() }, 3000 );
+}
 function winner()
 {
     for ( let i = 1; i < 10; i++ )
@@ -55,11 +70,7 @@ function winner()
     if ( er[ 1 ] != '' && er[ 2 ] != '' && er[ 3 ] != '' && er[ 4 ] != '' && er[ 5 ] != '' && er[ 6 ] != '' && er[ 7 ] != '' && er[ 8 ] != '' && er[ 9 ] != '' && f === 0 )
     {
         title.innerHTML = 'Equal';
-        yt(1, 2, 3 );
-        yt(4, 5, 6 );
-        yt(7, 8, 9 );
-        setInterval( function () { title.innerHTML += '.' }, 1000 );
-        setTimeout( function () { location.reload() },3000 );
+        ytt(1, 2, 3,4 ,5, 6, 7, 8, 9 );
     }
 }
 
